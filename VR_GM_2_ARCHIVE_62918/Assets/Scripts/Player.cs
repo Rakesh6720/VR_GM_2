577ee3bcf5b5d1a0f63d5bba675f1dd7465e85be
Rakesh Baruah <rbaruah@gmail.com>
2018-06-29 14:52:29 -0500

    this updates a second more recent archive with linear regression algorithms

---
+++ VR_GM_2_ARCHIVE_62918/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+
+    public int ID;
+    public int year;
+    public string name;
+    public string firstName;
+    public string lastName;
+    public string position;
+    public int heightFeet;
+    public int heightInches;
+    public float heightInchesTotal;
+    public int weight;
+    public float fortyYd;
+    public float twentySs;
+    public float threeCone;
+    public float vertical;
+    public int broad;
+    public int bench;
+    public int round;
+    public string college;
+    public int pick;
+    public int pickRound;
+    public int pickTotal;
+    public int wonderLick;
+    public float nflGrade;
+
+}
+
